61872cbebdde7528f44765b698c820f7a275186d
Niremizov <niremizov@yandex.ru>
2018-04-09 00:27:35 +0300

    Fixed save error.

--- lib/custom_redmine_links/repository_patch.rb
+++ lib/custom_redmine_links/repository_patch.rb
@@ -1,4 +1,6 @@
 Repository.class_eval do
+  safe_attributes 'extra_remote_pull_request_url'
+
   def extra_remote_pull_request_url(pull_request_id = nil)
     info = extra_info || {}
     url = info['extra_remote_pull_request_url'] || ''
